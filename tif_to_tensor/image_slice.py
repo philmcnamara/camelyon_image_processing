cf45cc7306dd930891587b0d9e59b007ab7eaf7c
Phil McNamara <phil.j.mcnamara@gmail.com>
2018-12-11 17:40:16 -0800

    updates to image_slice.py

--- tif_to_tensor/image_slice.py
+++ tif_to_tensor/image_slice.py
@@ -1,4 +1,4 @@
-#!/packages/anaconda3/5.1.0/bin/python3
+#!/usr/bin/env python3
 
 import openslide as os
 import argparse
@@ -6,34 +6,49 @@ import argparse
 
 def get_arguments():
     parser = argparse.ArgumentParser(
-        description="Splits a TIF file into 256*256 tiles")
-    parser.add_argument("-i", "--input_path", help="file input path",
+        description="Splits a TIF file into tiles")
+    parser.add_argument("-i", "--input_path", help="TIF file path",
                         required=True, type=str)
+    parser.add_argument("-o", "--output_path",
+                        help="Output directory (absolute path required)",
+                        required=True, type=str)
+    parser.add_argument("-t", "--tile_size",
+                        help="height/width of tiles (default 256)",
+                        required=False, type=int, default=256)
     return parser.parse_args()
 
 
-args = get_arguments()
+def main():
+    args = get_arguments()
+    image = os.OpenSlide(args.input_path)
+    outpath = args.output_path
+    tile_size = args.tile_size
 
-image = args.input_path
+    # Fix possible inconsistency in user-inputted outpath
+    outpath = outpath.rstrip('/')
 
-# Write to SSD to speed things up, copy data afterwards
-outpath = "/tmp/sliced/"
+    # Get the image name from the input file path and extension
+    image_name = args.input_path.split('/')[-1].split(".")[0]
 
-osr = os.OpenSlide(image)
-
-current_x = 0
-current_y = 0
-
-max_x = osr.dimensions[0]
-max_y = osr.dimensions[1]
-
-slice_size = (256, 256)
-
-while current_y < max_y:
-    while current_x < max_x:
-        target = osr.read_region((current_x, current_y), 0, slice_size)
-        target.save(outpath + "tumor_110" + "_" + str(current_x) + "_" +
-                    str(current_y) + ".png")
-        current_x += 256
+    # Initialize at top left corner of image
     current_x = 0
-    current_y += 256
+    current_y = 0
+
+    max_x = image.dimensions[0]
+    max_y = image.dimensions[1]
+
+    # Openslide.read_region requires a tuple input
+    slice_size = (tile_size, tile_size)
+
+    while current_y < max_y:
+        while current_x < max_x:
+            target = image.read_region((current_x, current_y), 0, slice_size)
+            target.save(outpath + "/" + image_name + "_" + str(current_x) +
+                        "_" + str(current_y) + ".png")
+            current_x += tile_size
+        current_x = 0
+        current_y += tile_size
+
+
+if __name__ == "__main__":
+    main()
